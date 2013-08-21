f7fc6063222ddd47b7cd35de7cabdfbd91f93f24
Victor Vasiliev <vasilvv@gmail.com>
2013-08-20 22:32:10 -0400

    Add some basic list of transactions.

--- ndsc.py
+++ ndsc.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # 
 # This is a very simple discuss client which was created to make reading discuss
-# meetings less painful
+# meetings less painful. It is sort of ugly and contains globals.
 #
 
 from __future__ import division
 from __future__ import print_function
 
 import curses, discuss  # discuss always comes with curses
+
 import argparse
+import locale
 import signal
 import sys
 
-def die(reason):
+# Globals initialization
+pos_cur = 1
+pos_top = 1
+
+# Functions
+
+def die(reason = ""):
     try:
         screen.keypad(0)
         curses.nocbreak()
@@ -22,15 +31,17 @@ def die(reason):
     except:
         pass
 
-    print(reason, file=sys.stderr)
-    sys.exit(1)
+    if screen != "":
+        print(reason, file=sys.stderr)
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
 class ProgressDisplay(object):
     def __init__(self):
         self.last_text = ""
         curses.curs_set(0)
         screen.nodelay(True)
-        screen.keypad(0)
 
     def display_progress(this, cur, total, left):
         done = total - left
@@ -65,11 +76,70 @@ def init_ui():
     screen = curses.initscr()
     curses.noecho()
     curses.cbreak()
-    screen.keypad(1)
+
+def redraw():
+    global pos_cur, pos_top
+
+    max_y, max_x = screen.getmaxyx()
+    rows = max_y - 6
+
+    # Scrolling down
+    if pos_cur - pos_top > rows:
+        pos_top += 1
+    # Resizing or scrolling up
+    if pos_cur - pos_top > rows or pos_cur < pos_top:
+        pos_top = pos_cur
+
+    screen.erase()
+    
+    # Corners of the main window
+    screen.addstr(1, 1, '┌')
+    screen.addstr(max_y-2, 1, '└')
+    screen.addstr(1, max_x-2, '┐')
+    screen.addstr(max_y-2, max_x-2, '┘')
+
+    # Borders of the main window
+    for i in range(2, max_y-2):
+        screen.addstr(i, 1, '│')
+        screen.addstr(i, max_x-2, '│')
+    for i in range(2, max_x-2):
+        screen.addstr(1, i, '─')
+        screen.addstr(max_y-2, i, '─')
+
+    current_transactions = transactions[pos_top:pos_top + rows + 1]
+
+    for i in range(0, len(current_transactions)):
+        cur = pos_cur - pos_top == i
+        trn = current_transactions[i]
+        if cur:
+            spaces = " " * (max_x - 6 - len(trn.subject))
+            screen.addstr(2 + i, 3, trn.subject + spaces, curses.A_REVERSE)
+        else:
+            screen.addstr(2 + i, 3, trn.subject)
+
+def main_loop():
+    global pos_cur, pos_top
+
+    screen.nodelay(True)
+    screen.keypad(True)
+    redraw()
+
+    while True:
+        ch = screen.getch()
+
+        if ch == ord('q'):
+            return
+        if ch == curses.KEY_DOWN:
+            pos_cur += 1
+        if ch == curses.KEY_UP:
+            pos_cur -= 1
+
+        redraw()
 
 def main():
     global name, server, path
 
+    locale.setlocale(locale.LC_ALL, '')
     arg_parser = argparse.ArgumentParser(description="Discuss meeting viewer")
     arg_parser.add_argument('meeting_name', help="Name of the meeting to view")
 
@@ -85,7 +155,8 @@ def main():
     init_ui()
     init_meeting()
 
-    die("Test")
+    main_loop()
+    die()
 
 if __name__ == '__main__':
     main()
